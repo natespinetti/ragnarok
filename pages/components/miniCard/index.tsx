b639d8d219ebc1b62d5371a692f7d443e92e3efd
Nathan Spinetti <104846459+natespinetti@users.noreply.github.com>
2023-05-04 11:40:19 -0400

    Refactor isDisabled buttons

--- pages/components/miniCard/index.tsx
+++ pages/components/miniCard/index.tsx
@@ -17,7 +17,7 @@ export const MiniCard = ({ fields }: TypeMiniCard) => {
   return (
       <Box display="block" w="100%" alignItems="center">
           <Box pos="relative">
-            <Link isDisabled={!link} cursor={!link ? "not-allowed" : "pointer"} href={link} w="100%" display="grid" onMouseOver={over} onMouseOut={out} mb={{base: '6', xl: '0'}}>
+            <Link href={link} w="100%" display="grid" onMouseOver={over} onMouseOut={out} mb={{base: '6', xl: '0'}}>
               <Box borderRadius="25px" pos="absolute" display="flex" w="100%" h="100%" transition=".4s ease-in-out" bg="brand.gray" opacity={isVisible ? ".8" : ".5"}></Box>
               <Text lineHeight="1" pos="absolute" display="flex" justifySelf="center" alignSelf="center" justifyContent="center" alignItems="center" fontSize={{ base: "2xl", md: "4xl"}} fontWeight="900" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
               <Text lineHeight="1" pos="absolute" justifySelf="center" alignSelf="center" transition=".4s ease-in-out" transform={isVisible ? "translateY(-85%)" : "translateY(0)"} visibility={isVisible ? "visible" : "hidden"} justifyContent="center" alignItems="center" fontSize={{ base: "2xl", md: "4xl"}} fontWeight="900" display="flex" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
@@ -28,8 +28,14 @@ export const MiniCard = ({ fields }: TypeMiniCard) => {
           <Box pos="relative" pt="1rem">
             <Text fontSize="3xl" fontWeight="700" mb="2">{headline}</Text>
             <Text fontSize="lg" mb="4">{summary}</Text>
-            <Link isDisabled={!link} cursor={!link ? "not-allowed" : "pointer"} href={link}><Button bg={!link ? "brand.grays.500" : "brand.gray"} cursor={!link ? "not-allowed" : "pointer"} color="brand.white" _hover={{ bg: "brand.grays.500" }}>{linkText ?? 'View Project'}</Button></Link>
-          </Box>
+            {link ? (
+              <Link href={link} w={{base: '100%', xl: 'fit-content'}} display="grid" onMouseOver={over} onMouseOut={out} mb={{base: '6', xl: '0'}}>
+                <Button bg={!link ? "brand.grays.500" : "brand.gray"} color="brand.white" _hover={{ bg: "brand.grays.500" }}>{linkText ?? 'View Project'}</Button>
+              </Link>
+            ) : (
+              <Button w={{base: '100%', xl: 'fit-content'}} cursor="not-allowed" isDisabled bg={!link ? "brand.grays.500" : "brand.gray"} color="brand.white" _hover={{ bg: "brand.grays.500" }}>{linkText ?? 'View Project'}</Button>
+            )}
+        </Box>
       </Box>
   );
 };
