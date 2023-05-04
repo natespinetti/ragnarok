70ea41337ce29dfae8c8270f4e9c30cedbe95532
Nathan Spinetti <104846459+natespinetti@users.noreply.github.com>
2023-05-04 16:00:54 -0400

    Card text centering from grid to flex

--- pages/components/card/index.tsx
+++ pages/components/card/index.tsx
@@ -18,11 +18,11 @@ export const Card = ({ fields }: TypeCard) => {
     <Container w={['90%', '80%']} maxW="1200px" p="2rem 0">
       <Box display={{ lg:'block', xl:'grid'}} w="100%" alignItems="center" gridTemplateColumns="1fr 1fr" gridGap="3rem">
           <Box order={whichSide ? "1" : "2"} pos="relative">
-            <Link href={link} w="100%" display="grid" onMouseOver={over} onMouseOut={out} mb={{base: '6', xl: '0'}}>
+            <Link href={link} w="100%" display="flex" justifyContent="center" alignItems="center" onMouseOver={over} onMouseOut={out} mb={{base: '6', xl: '0'}}>
               <Box borderRadius="25px" pos="absolute" display="flex" w="100%" h="100%" transition=".4s ease-in-out" bg="brand.gray" opacity={isVisible ? ".8" : ".5"}></Box>
-              <Text lineHeight="1" pos="absolute" display="flex" justifySelf="center" alignSelf="center" justifyContent="center" alignItems="center" fontSize={{ base: "4xl", md: "6xl"}} fontWeight="900" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
-              <Text lineHeight="1" pos="absolute" justifySelf="center" alignSelf="center" transition=".4s ease-in-out" transform={isVisible ? "translateY(-85%)" : "translateY(0)"} visibility={isVisible ? "visible" : "hidden"} justifyContent="center" alignItems="center" fontSize={{ base: "4xl", md: "6xl"}} fontWeight="900" display="flex" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
-              <Text lineHeight="1" pos="absolute" justifySelf="center" alignSelf="center" transition=".4s ease-in-out" transform={isVisible ? "translateY(85%)" : "translateY(0)"} visibility={isVisible ? "visible" : "hidden"} justifyContent="center" alignItems="center" fontSize={{ base: "4xl", md: "6xl"}} fontWeight="900" display="flex" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
+              <Text lineHeight="1" pos="absolute" fontSize={{ base: "4xl", md: "6xl"}} fontWeight="900" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
+              <Text lineHeight="1" pos="absolute" transition=".4s ease-in-out" transform={isVisible ? "translateY(-85%)" : "translateY(0)"} visibility={isVisible ? "visible" : "hidden"} fontSize={{ base: "4xl", md: "6xl"}} fontWeight="900" display="flex" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
+              <Text lineHeight="1" pos="absolute" transition=".4s ease-in-out" transform={isVisible ? "translateY(85%)" : "translateY(0)"} visibility={isVisible ? "visible" : "hidden"} fontSize={{ base: "4xl", md: "6xl"}} fontWeight="900" display="flex" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
               <Image src={image?.fields.file.url} alt={image?.fields.title} w="100%" h="100%" borderRadius="25px"/>
             </Link>
           </Box>
--- pages/components/miniCard/index.tsx
+++ pages/components/miniCard/index.tsx
@@ -17,11 +17,11 @@ export const MiniCard = ({ fields }: TypeMiniCard) => {
   return (
       <Box display="block" w="100%" alignItems="center">
           <Box pos="relative">
-            <Link href={link} w="100%" display="grid" onMouseOver={over} onMouseOut={out} mb={{base: '6', xl: '0'}}>
+            <Link href={link} w="100%" display="flex" justifyContent="center" alignItems="center" onMouseOver={over} onMouseOut={out} mb={{base: '6', xl: '0'}}>
               <Box borderRadius="25px" pos="absolute" display="flex" w="100%" h="100%" transition=".4s ease-in-out" bg="brand.gray" opacity={isVisible ? ".8" : ".5"}></Box>
-              <Text lineHeight="1" pos="absolute" display="flex" justifySelf="center" alignSelf="center" justifyContent="center" alignItems="center" fontSize={{ base: "2xl", md: "4xl"}} fontWeight="900" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
-              <Text lineHeight="1" pos="absolute" justifySelf="center" alignSelf="center" transition=".4s ease-in-out" transform={isVisible ? "translateY(-85%)" : "translateY(0)"} visibility={isVisible ? "visible" : "hidden"} justifyContent="center" alignItems="center" fontSize={{ base: "2xl", md: "4xl"}} fontWeight="900" display="flex" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
-              <Text lineHeight="1" pos="absolute" justifySelf="center" alignSelf="center" transition=".4s ease-in-out" transform={isVisible ? "translateY(85%)" : "translateY(0)"} visibility={isVisible ? "visible" : "hidden"} justifyContent="center" alignItems="center" fontSize={{ base: "2xl", md: "4xl"}} fontWeight="900" display="flex" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
+              <Text lineHeight="1" pos="absolute" fontSize={{ base: "2xl", md: "4xl"}} fontWeight="900" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
+              <Text lineHeight="1" pos="absolute" transition=".4s ease-in-out" transform={isVisible ? "translateY(-85%)" : "translateY(0)"} visibility={isVisible ? "visible" : "hidden"} fontSize={{ base: "2xl", md: "4xl"}} fontWeight="900" display="flex" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
+              <Text lineHeight="1" pos="absolute" transition=".4s ease-in-out" transform={isVisible ? "translateY(85%)" : "translateY(0)"} visibility={isVisible ? "visible" : "hidden"} fontSize={{ base: "2xl", md: "4xl"}} fontWeight="900" display="flex" color="brand.white" borderLeftColor="brand.yellow !important" borderLeft="5px solid" pl="5">{textOverImage}</Text>
               <Image src={image?.fields.file.url} alt={image?.fields.title} w="100%" h="100%" borderRadius="25px"/>
             </Link>
           </Box>
