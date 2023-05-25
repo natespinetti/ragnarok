5a3eab744b7a75ced6df0f56f7042b183d8b58a8
Nathan Spinetti <104846459+natespinetti@users.noreply.github.com>
2023-05-25 15:01:46 -0400

    Redirects

--- next.config.js
+++ next.config.js
@@ -1,53 +1,51 @@
-const { createRedirect } = require('next-redirect');
-
 module.exports = {
-  async rewrites() {
+  async redirects() {
     return [
-      createRedirect({
+      {
         source: '/canvas',
         destination: '/',
-        statusCode: 301, 
-      }),
-      createRedirect({
+        permanent: true,
+      },
+      {
         source: '/contact',
         destination: '/about',
-        statusCode: 301,
-      }),
-      createRedirect({
+        permanent: true,
+      },
+      {
         source: '/buongiorno',
         destination: 'https://buongiorno-lilac.vercel.app/',
-        statusCode: 301,
-      }),
-      createRedirect({
+        permanent: true,
+      },
+      {
         source: '/visionary',
         destination: 'https://xd.adobe.com/view/33551c2d-dd7b-4b26-427f-fc11172e2a34-4564/?fullscreen',
-        statusCode: 301,
-      }),
-      createRedirect({
+        permanent: true,
+      },
+      {
         source: '/bojack-horseman',
         destination: 'https://vimeo.com/332264587',
-        statusCode: 301,
-      }),
-      createRedirect({
+        permanent: true,
+      },
+      {
         source: '/tim-cox',
         destination: 'https://vimeo.com/328321554',
-        statusCode: 301,
-      }),
-      createRedirect({
+        permanent: true,
+      },
+      {
         source: '/hollow-city',
         destination: 'https://www.nathanspinetti.com/HollowCity.pdf',
-        statusCode: 301,
-      }),
-      createRedirect({
+        permanent: true,
+      },
+      {
         source: '/dodough',
         destination: 'https://www.nathanspinetti.com/DoDough.pdf',
-        statusCode: 301,
-      }),
-      createRedirect({
+        permanent: true,
+      },
+      {
         source: '/binging-with',
         destination: 'http://bingeworthy.infinityfreeapp.com/',
-        statusCode: 301,
-      }),
+        permanent: true,
+      },
     ];
   },
   reactStrictMode: true,
